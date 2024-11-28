1c6d758600060ffa392039742e5913a7ef5b1e7d
udonwajnr <udonwajnr10@gmail.com>
2024-11-28 11:05:53 +0100

    commit

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require("express");
-const http = require("http"); // Import HTTP module
-const app = express();
-const server = http.createServer(app); // Create a server from the express app
-// const connectDb = require("./config/db");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
-let cors = require("cors");
-let cookieParser = require("cookie-parser");
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+const connectDb = require("./config/db")
+const color = require("colors")
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+let cors = require("cors")
+const port=3000
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`.yellow);
-    console.log(new Date());
-    // sendMedicationReminder(userPhoneNumber, userName, medicationName, dosage, dosageForm);
-  });
+app.use(cookieParser())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// app.use("/api/hospital",require("./route/hospitalRoute"))
+
+app.listen(port,()=>{
+    console.log("I'm back")
+})
+
+connectDb()
