f36a4534b5e57d1d737ebb710ced72761970d72e
udonwajnr <udonwajnr10@gmail.com>
2024-12-16 22:03:02 +0100

    commit

--- controllers/userAuthentication.js
+++ controllers/userAuthentication.js
@@ -205,6 +205,7 @@ const loginUser = asyncHandler(async (req, res) => {
       id: user._id,
       username: user.username,
       email: user.email,
+      role:user.role,
       accessToken,
     });
 });
