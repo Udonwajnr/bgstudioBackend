e645b1c4a3b1a21a9a46494cce059baa1d5c6908
udonwajnr <udonwajnr10@gmail.com>
2025-01-23 22:54:54 +0100

    commit

--- middleware/customerMiddleware.js
+++ middleware/customerMiddleware.js
@@ -3,8 +3,13 @@ const asyncHandler = require("express-async-handler");
 const Customer = require("../models/Customer"); // Adjust the path to your Customer model
 
 const protect = asyncHandler(async (req, res, next) => {
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    // If the user is authenticated via Passport session
+    req.user = req.user; // Passport automatically attaches the user to `req.user`
+    return next();
+  }
+
   let token;
-  // Check if the token is provided in the Authorization header
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
@@ -20,15 +25,16 @@ const protect = asyncHandler(async (req, res, next) => {
         throw new Error("User not found");
       }
 
-      next(); // Continue to the next middleware or route handler
+      return next(); // Continue to the next middleware or route handler
     } catch (error) {
       res.status(401);
       throw new Error("Not authorized, invalid token");
     }
-  } else {
-    res.status(401);
-    throw new Error("Not authorized, no token provided");
   }
+
+  // If neither session nor JWT is valid
+  res.status(401);
+  throw new Error("Not authorized, no token or session provided");
 });
 
 module.exports = { protect };
