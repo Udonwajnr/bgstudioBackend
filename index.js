08baab6eace1e057508b502578cdba7fb786be37
udonwajnr <udonwajnr10@gmail.com>
2025-01-26 07:29:27 +0100

    commit

--- index.js
+++ index.js
@@ -61,10 +61,10 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  console.log('Session data:', req.session); // Log session data on every request
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Session data:', req.session); // Log session data on every request
+//   next();
+// });
 
 // Passport middleware
 app.use(passport.initialize());
