0efd9eb412fafb37aa42e1e72c6b4796b210284b
udonwajnr <udonwajnr10@gmail.com>
2025-01-28 06:11:07 +0100

    commit

--- config/passport.js
+++ config/passport.js
@@ -45,7 +45,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `http://localhost:8000/auth/google/callback`,
+      callbackURL: `https://bgstudiobackend-1.onrender.com/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
